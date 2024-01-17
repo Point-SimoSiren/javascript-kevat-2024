47deac73c9cf4af4a9da83ba7998dc3629bf6e8b
Simo Sirén <simo.siren1@careeria.fi>
2024-01-17 14:54:23 +0200

    2 päivä valmis. Hakukoodi rikki.

---
+++ oliot.js
@@ -0,0 +1,44 @@
+
+var autot = [{id: 1, merkki: "Toyota", vuosimalli: 2006, hinta: 4500},
+ {id: 2, merkki: "BMW", vuosimalli: 1986, hinta: 3500},
+ {id: 3, merkki: "Lada", vuosimalli: 1991, hinta: 500}]
+
+var hakusana = ""
+
+ var standard_input = process.stdin
+ standard_input.setEncoding('utf-8')
+ console.log("JavaScript autohaku")
+ console.log("Sovellus suljetaan kirjoittamalla 'exit'")
+ process.stdout.write("Hae autoja merkin mukaan: ")
+ standard_input.on('data', function (data) {
+    // Sovellus suljetaan, kun käyttäjää antaa avainsanan "exit"
+
+    if (data === 'exit\r\n'){
+    console.log("Kiitos ja näkemiin.")
+    process.exit()
+    } 
+    
+    else {
+        hakusana = data
+        autot.forEach(a => 
+       
+        )
+    }}
+ )
+
+// autot.forEach(auto => {
+//     // Black friday: poista kommentti
+//     //auto.hinta = 100
+//     console.log(auto.merkki + " vuodelta " + auto.vuosimalli)
+//     console.log("Hinta: " + auto.hinta)
+//     console.log("___________________________________________________")
+// }
+// )
+
+// autot.forEach(auto => {
+//     if (auto.id === 3) {
+//     console.log("Auto id:llä " + auto.id + " on LUJAA LAATUA " + auto.merkki )
+//     console.log("___________________________________________________")
+//     }
+// }
+
--- sivu.js
+++ sivu.js
@@ -2,11 +2,54 @@
 // Palvelut sivun näyttäminen
 function palvelut() {
     let palvelut = `<h2>Palvelumme</h2>
-    
             <p>Eläinlääkäri</p>
             <p>Päivähoito</p>
             <p>Yöhoito</p>
-    
     `
     document.getElementById("sisältö").innerHTML = palvelut
 }
+
+// Yhteystiedot
+function yhteystiedot() {
+
+    document.getElementById("sisältö").innerHTML = ""
+    var otsikko = document.createElement("h2")
+    var teksti = document.createTextNode("Yhteystiedot")
+    otsikko.appendChild(teksti)
+    var sisältö = document.getElementById("sisältö")
+    sisältö.appendChild(otsikko)
+
+    var puhelin = document.createElement("h4")
+    var puhTeksti = document.createTextNode("P. 05082380928")
+    puhelin.appendChild(puhTeksti)
+    sisältö.appendChild(puhelin)
+
+}
+
+function tumma() {
+    document.getElementById("sivu").style.backgroundColor = "black"
+    document.getElementById("sivu").style.color = "silver"
+    localStorage.setItem("teema", "tumma")
+}
+
+function vaalea() {
+    document.getElementById("sivu").style.backgroundColor = "white"
+    document.getElementById("sivu").style.color = "black"
+    localStorage.setItem("teema", "vaalea")
+}
+
+// Suoritetaan aina alussa
+if (localStorage.getItem("teema") == "tumma") {
+    tumma()
+}
+else {
+    vaalea()
+}
+
+//localStorage.clear()
+
+
+
+
+
+
